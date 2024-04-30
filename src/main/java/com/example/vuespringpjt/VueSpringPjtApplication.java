c108ea2efa30bb4d6fc5c1517a41f5c7186b83d8
yk <lysel1225@naver.com>
2024-04-30 09:16:41 +0900

    first commit

---
+++ src/main/java/com/example/vuespringpjt/VueSpringPjtApplication.java
@@ -0,0 +1,13 @@
+package com.example.vuespringpjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueSpringPjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueSpringPjtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vuespringpjt/VueSpringPjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vuespringpjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueSpringPjtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
